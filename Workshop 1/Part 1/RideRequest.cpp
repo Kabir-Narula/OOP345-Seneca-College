5a7044736f99619fc62e986cf15542eec3ebe4e0
Pupuu <137004973+Kim-Chaewonn@users.noreply.github.com>
2023-11-26 18:51:01 -0500

    Update RideRequest.cpp

--- Workshop 1/Part 1/RideRequest.cpp
+++ Workshop 1/Part 1/RideRequest.cpp
@@ -1,12 +1,5 @@
 
-//==============================================
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-//==============================================
-//I did all the code by myself and only copied the code given by professor 
-//provided to complete my workshops and assignments.
+
 #include "RideRequest.h"
 
 using namespace std;
