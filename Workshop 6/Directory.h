fcb6882dcdcb86c67d0d572103554565c278e1ff
Pupuu <137004973+Kim-Chaewonn@users.noreply.github.com>
2023-11-26 18:59:42 -0500

    Update Directory.h

--- Workshop 6/Directory.h
+++ Workshop 6/Directory.h
@@ -1,10 +1,3 @@
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-//==============================================
-//I did all the code by myself and only copied the code given by professor 
-//provided to complete my workshops and assignments.
 
 #pragma once
 #ifndef SDDS_DIRECTORY_H
