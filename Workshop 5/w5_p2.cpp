ee106e24e51cc9e51d6d44adbeeeb9c6b397f0ac
Pupuu <137004973+Kim-Chaewonn@users.noreply.github.com>
2023-11-26 18:56:34 -0500

    Update w5_p2.cpp

--- Workshop 5/w5_p2.cpp
+++ Workshop 5/w5_p2.cpp
@@ -1,10 +1,4 @@
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-//==============================================
-//I did all the code by myself and only copied the code given by professor 
-//provided to complete my workshops and assignments.
+
 
 // Workshop 5 - Functions and Error Handling
 // 2020/02 - Cornel
