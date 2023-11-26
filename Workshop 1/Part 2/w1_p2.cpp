bc17257ba6c08980181a0c8011c8a8dbea2fea08
Pupuu <137004973+Kim-Chaewonn@users.noreply.github.com>
2023-11-26 18:52:00 -0500

    Update w1_p2.cpp

--- Workshop 1/Part 2/w1_p2.cpp
+++ Workshop 1/Part 2/w1_p2.cpp
@@ -1,14 +1,4 @@
-// Workshop 1 - Linkage, Storage Duration, Namespaces, and OS Interface
-// Hina Tariq - 2023/03/06
-// Cornel - 2023/09/08
-//==============================================
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-//==============================================
-//I did all the code by myself and only copied the code given by professor 
-//provided to complete my workshops and assignments.
+
 
 #include <iostream>
 #include <iomanip>
