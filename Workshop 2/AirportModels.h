e8de5d06946c0f27d1abdea1a4d02cc60302fa2b
Pupuu <137004973+Kim-Chaewonn@users.noreply.github.com>
2023-11-26 18:52:20 -0500

    Update AirportModels.h

--- Workshop 2/AirportModels.h
+++ Workshop 2/AirportModels.h
@@ -1,11 +1,4 @@
 
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-//==============================================
-// I did all the code by myself and only copied the code given by the professor
-// provided to complete my workshops and assignments.
 
 #ifndef SDDS_AIRPORTMODELS_H
 #define SDDS_AIRPORTMODELS_H
