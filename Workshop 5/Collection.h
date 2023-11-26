e08a48c281728cfaa6e4532461589d0f511c73e8
Pupuu <137004973+Kim-Chaewonn@users.noreply.github.com>
2023-11-26 18:55:49 -0500

    Update Collection.h

--- Workshop 5/Collection.h
+++ Workshop 5/Collection.h
@@ -1,10 +1,3 @@
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-//==============================================
-//I did all the code by myself and only copied the code given by professor 
-//provided to complete my workshops and assignments.
 
 #ifndef SDDS_COLLECTION_H
 #define SDDS_COLLECTION_H
