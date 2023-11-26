e6abcbe71a85b4384e882be1e1f640539c39621f
Pupuu <137004973+Kim-Chaewonn@users.noreply.github.com>
2023-11-26 18:53:16 -0500

    Update Book.h

--- Workshop 3/Book.h
+++ Workshop 3/Book.h
@@ -1,11 +1,3 @@
-//==============================================
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-//==============================================
-//I did all the code by myself and only copied the code given by professor 
-//provided to complete my workshops and assignments.
 
 #ifndef SDDS_BOOK_H
 #define SDDS_BOOK_H
